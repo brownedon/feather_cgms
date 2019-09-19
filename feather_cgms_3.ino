576a21d36175e47d0cc6fd5462730c3e233c7931
Don Browne <brownedon@comcast.net>
2019-09-19 15:30:22 -0400

    Add HRM Code
    Added code to let the feather appear as a Heart Rate sensor, sending glucose in place of heart rate.

--- feather_cgms_3.ino
+++ feather_cgms_3.ino
@@ -163,6 +163,20 @@ BLECharacteristic slope = BLECharacteristic(SLOPE_CHARACTERISTIC_UUID);
 BLECharacteristic intercept = BLECharacteristic(INTERCEPT_CHARACTERISTIC_UUID);
 BLECharacteristic battery = BLECharacteristic(BATTERY_CHARACTERISTIC_UUID);
 
+
+
+//heart rate
+//NOTE: at the moment you will only be able to act as 1 peripheral at a time
+//you can either pair with a HRM or with an iPhone, not both
+/* HRM Service Definitions
+   Heart Rate Monitor Service:  0x180D
+   Heart Rate Measurement Char: 0x2A37
+   Body Sensor Location Char:   0x2A38
+*/
+BLEService        hrms = BLEService(UUID16_SVC_HEART_RATE);
+BLECharacteristic hrmc = BLECharacteristic(UUID16_CHR_HEART_RATE_MEASUREMENT);
+BLECharacteristic bslc = BLECharacteristic(UUID16_CHR_BODY_SENSOR_LOCATION);
+
 //
 // end peripheral
 //
@@ -172,8 +186,8 @@ BLECharacteristic battery = BLECharacteristic(BATTERY_CHARACTERISTIC_UUID);
 void setup()
 {
   Serial.begin(115200);
-  // Initialize Bluefruit with max concurrent connections as Peripheral = 1, Central = 1
-  Bluefruit.begin(1, 1);
+  // Initialize Bluefruit with max concurrent connections as Peripheral = 2, Central = 1
+  Bluefruit.begin(2, 1);
 
   // Initialize Internal File System
   InternalFS.begin();
@@ -184,16 +198,18 @@ void setup()
   Bluefruit.setTxPower(4);
   Bluefruit.setName("Bluefruit CGMS");
   //Bluefruit.Periph.setConnInterval(9, 24);
-  Bluefruit.Periph.setConnInterval(160,80);
+  Bluefruit.Periph.setConnInterval(160, 80);
   // Callbacks for Peripheral
   Bluefruit.Periph.setConnectCallback(prph_connect_callback);
   Bluefruit.Periph.setDisconnectCallback(prph_disconnect_callback);
 
   setup_cc2500();
   // Set up and start advertising
+  setupHRM();
   startAdv();
   setup_mi();
   setupCGMS();
+
   initReadings();
   Scheduler.startLoop(loop_cc2500);
   //this sets up nffs for the first time
@@ -201,6 +217,67 @@ void setup()
     writeSlope();
     writeIntercept();
   }
+}
+
+void setupHRM(void)
+{
+  // Configure the Heart Rate Monitor service
+  // See: https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.heart_rate.xml
+  // Supported Characteristics:
+  // Name                         UUID    Requirement Properties
+  // ---------------------------- ------  ----------- ----------
+  // Heart Rate Measurement       0x2A37  Mandatory   Notify
+  // Body Sensor Location         0x2A38  Optional    Read
+  // Heart Rate Control Point     0x2A39  Conditional Write       <-- Not used here
+  hrms.begin();
+
+  // Note: You must call .begin() on the BLEService before calling .begin() on
+  // any characteristic(s) within that service definition.. Calling .begin() on
+  // a BLECharacteristic will cause it to be added to the last BLEService that
+  // was 'begin()'ed!
+
+  // Configure the Heart Rate Measurement characteristic
+  // See: https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.heart_rate_measurement.xml
+  // Properties = Notify
+  // Min Len    = 1
+  // Max Len    = 8
+  //    B0      = UINT8  - Flag (MANDATORY)
+  //      b5:7  = Reserved
+  //      b4    = RR-Internal (0 = Not present, 1 = Present)
+  //      b3    = Energy expended status (0 = Not present, 1 = Present)
+  //      b1:2  = Sensor contact status (0+1 = Not supported, 2 = Supported but contact not detected, 3 = Supported and detected)
+  //      b0    = Value format (0 = UINT8, 1 = UINT16)
+  //    B1      = UINT8  - 8-bit heart rate measurement value in BPM
+  //    B2:3    = UINT16 - 16-bit heart rate measurement value in BPM
+  //    B4:5    = UINT16 - Energy expended in joules
+  //    B6:7    = UINT16 - RR Internal (1/1024 second resolution)
+  hrmc.setProperties(CHR_PROPS_NOTIFY);
+  hrmc.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  hrmc.setFixedLen(2);
+  hrmc.setCccdWriteCallback(cccd_callback3);  // Optionally capture CCCD updates
+  hrmc.begin();
+  uint8_t hrmdata[2] = { 0b00000110, 0x40 }; // Set the characteristic to use 8-bit values, with the sensor connected and detected
+  hrmc.write(hrmdata, 2);
+
+  // Configure the Body Sensor Location characteristic
+  // See: https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.body_sensor_location.xml
+  // Properties = Read
+  // Min Len    = 1
+  // Max Len    = 1
+  //    B0      = UINT8 - Body Sensor Location
+  //      0     = Other
+  //      1     = Chest
+  //      2     = Wrist
+  //      3     = Finger
+  //      4     = Hand
+  //      5     = Ear Lobe
+  //      6     = Foot
+  //      7:255 = Reserved
+  bslc.setProperties(CHR_PROPS_READ);
+  bslc.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  bslc.setFixedLen(1);
+  bslc.begin();
+  bslc.write8(2);    // Set the characteristic to 'Wrist' (2)
 }
 
 void setup_mi() {
@@ -220,8 +297,8 @@ void setup_mi() {
   Bluefruit.Central.setConnectCallback(cent_connect_callback);
   Bluefruit.Scanner.restartOnDisconnect(true);
   Bluefruit.Scanner.filterRssi(-100);
- // Bluefruit.Scanner.setInterval(32, 244);       // in units of 0.625 ms
-   Bluefruit.Scanner.setInterval(160, 80);       // in units of 0.625 ms
+  // Bluefruit.Scanner.setInterval(32, 244);       // in units of 0.625 ms
+  Bluefruit.Scanner.setInterval(160, 80);       // in units of 0.625 ms
   Bluefruit.Scanner.useActiveScan(true);        // Request scan response data
   Bluefruit.Scanner.start(0);
 
@@ -296,17 +373,34 @@ void cccd_callback2(uint16_t conn_hdl, BLECharacteristic* chr, uint16_t cccd_val
   Serial.println("");
 }
 
+void cccd_callback3(uint16_t conn_hdl, BLECharacteristic* chr, uint16_t cccd_value)//(BLECharacteristic& chr, uint16_t cccd_)
+{
+  // Display the raw request packet
+  Serial.print("CCCD Updated: ");
+  Serial.print(cccd_value);
+  Serial.println("");
+
+  if (chr->uuid == hrmc.uuid) {
+    if (chr->notifyEnabled(conn_hdl)) {
+      Serial.println("Heart Rate Measurement 'Notify' enabled");
+    } else {
+      Serial.println("Heart Rate Measurement 'Notify' disabled");
+    }
+  }
+}
 void startAdv(void)
 {
   // Advertising packet
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
   Bluefruit.Advertising.addService(cgms);
+  // Include HRM Service UUID
+  Bluefruit.Advertising.addService(hrms);
   Bluefruit.ScanResponse.addName();
   Bluefruit.Advertising.restartOnDisconnect(true);
   //if this doesn't match Bluefruit.Scanner.setInterval above, then this won't reconnect
- // Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
-  Bluefruit.Advertising.setInterval(160, 80); 
+  // Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+  Bluefruit.Advertising.setInterval(160, 80);
   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
   Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
 }
@@ -323,6 +417,7 @@ void loop()
   __set_FPSCR(__get_FPSCR()  & ~(FPU_EXCEPTION_MASK));
   (void) __get_FPSCR();
   NVIC_ClearPendingIRQ(FPU_IRQn);
+
   waitForEvent();
   delay(2000);
 }
@@ -403,6 +498,11 @@ void peripheral_comm() {
         writeIntercept();
       }
     }
+    //send "heart rate"
+    uint8_t hrmdata[2] = { 0b00000110, EST_GLUCOSE };
+    if ( hrmc.notify(hrmdata, sizeof(hrmdata)) ) {
+      Serial.print("Heart Rate Measurement(glucose) updated to: "); Serial.println(EST_GLUCOSE);
+    }
   }
 }
 
@@ -452,9 +552,9 @@ void do_messaging() {
       //reset although plugging in to charge probably cleared this anyway
       lowBatt == 0;
     }
-  }else{
-      Bluefruit.Scanner.start(0);
-    }
+  } else {
+    Bluefruit.Scanner.start(0);
+  }
 }
 void handle_isig() {
   Serial.println("handle_isig");
@@ -992,7 +1092,7 @@ void scan_callback(ble_gap_evt_adv_report_t* report)
   Serial.println(report->peer_addr.addr[5], HEX);
   if (report->peer_addr.addr[5] == 0xF2) {  //amazfit cor  FB
     //if (report->peer_addr.addr[5] == 0xFA) {  //dons mi
-   // if (report->peer_addr.addr[5] == 0xF8) {  //karins
+    // if (report->peer_addr.addr[5] == 0xF8) {  //karins
 
     // Connect to device
     Serial.println("Found device");
